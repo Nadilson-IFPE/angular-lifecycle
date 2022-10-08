cf0604af0f9790304af13bbfe1865fcdf4f8074d
Nadilson <nadilson@protonmail.com>
2022-10-08 01:51:24 -0300

    DoCheck e sua ordem de execução

---
+++ src/app/check-sample/check-sample.component.ts
@@ -0,0 +1,45 @@
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnInit,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-check-sample',
+  templateUrl: './check-sample.component.html',
+  styleUrls: ['./check-sample.component.css'],
+})
+export class CheckSampleComponent
+  implements
+    OnInit,
+    DoCheck,
+    AfterContentChecked,
+    AfterContentInit,
+    AfterViewChecked,
+    AfterViewInit
+{
+  constructor() {}
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+}
